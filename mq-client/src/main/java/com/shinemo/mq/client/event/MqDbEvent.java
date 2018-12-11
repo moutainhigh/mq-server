d58c29393ad61769f833815614c983de4f087c4d
zhangyan <578033593@qq.com>
2018-12-11 20:10:28 +0800

    fix

--- mq-client/src/main/java/com/shinemo/mq/client/event/MqDbEvent.java
+++ mq-client/src/main/java/com/shinemo/mq/client/event/MqDbEvent.java
@@ -1,4 +1,16 @@
 package com.shinemo.mq.client.event;
 
-public class MqDbEvent {
+import com.shinemo.mq.client.common.entity.BaseDO;
+import com.shinemo.mq.client.message.domain.MqFrom;
+import com.shinemo.mq.client.message.facade.MqMessageFacadeService;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MqDbEvent extends BaseDO {
+
+    private MqMessageFacadeService mqMessageFacadeService;
+    private MqFrom mqFrom;
+    private Integer type;
 }
--- mq-client/src/main/java/com/shinemo/mq/client/listener/MqDbEventListener.java
+++ mq-client/src/main/java/com/shinemo/mq/client/listener/MqDbEventListener.java
@@ -1,4 +1,26 @@
 package com.shinemo.mq.client.listener;
 
+import com.google.common.eventbus.Subscribe;
+import com.shinemo.mq.client.common.entity.InternalEventBus;
+import com.shinemo.mq.client.event.MqDbEvent;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class MqDbEventListener {
+
+    private InternalEventBus internalEventBus;
+
+    public void init(){
+        internalEventBus.register(this);
+    }
+
+    @Subscribe
+    public void onEvent(MqDbEvent event) {
+        // TODO 调用rpc接口
+        // 失败插入缓存队列重试
+        // TODO
+    }
+
 }
