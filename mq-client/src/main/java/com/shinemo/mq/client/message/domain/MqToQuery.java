c078ed2a96ea5e83b91273bec08c8c4206724e3c
zhangyan <578033593@qq.com>
2018-12-11 17:28:20 +0800

    fix

---
+++ mq-client/src/main/java/com/shinemo/mq/client/message/domain/MqToQuery.java
@@ -0,0 +1,15 @@
+package com.shinemo.mq.client.message.domain;
+
+
+import com.shinemo.mq.client.common.entity.BaseQuery;
+import lombok.Getter;
+import lombok.Setter;
+
+
+public class MqToQuery extends BaseQuery {
+
+	private static final long serialVersionUID = -2965882533176505094L;
+	private @Getter @Setter Long id;
+	private @Getter @Setter String messageId;
+	private @Getter @Setter String bizName;
+}
--- mq-client/src/main/java/com/shinemo/mq/client/message/facade/MqMessageFacadeService.java
+++ mq-client/src/main/java/com/shinemo/mq/client/message/facade/MqMessageFacadeService.java
@@ -1,5 +1,72 @@
 package com.shinemo.mq.client.message.facade;
 
+import com.shinemo.mq.client.common.list.ListWrapper;
+import com.shinemo.mq.client.common.result.Result;
+import com.shinemo.mq.client.message.domain.MqFrom;
+import com.shinemo.mq.client.message.domain.MqFromQuery;
+import com.shinemo.mq.client.message.domain.MqTo;
+import com.shinemo.mq.client.message.domain.MqToQuery;
+
+/**
+ * Mq消息message操作类
+ */
 public interface MqMessageFacadeService {
+
+    /**
+     * 插入生产的消息
+     * @param from
+     * @return
+     */
+    Result<MqFrom> insertMqFrom(MqFrom from);
+
+    /**
+     * 更新生产的消息
+     * @param from
+     * @return
+     */
+    Result<Void> updateMqFrom(MqFrom from);
+
+    /**
+     * 查询生产的消息 列表
+     * @param query
+     * @return
+     */
+    Result<ListWrapper<MqFrom>> findMqFrom(MqFromQuery query);
+
+    /**
+     * 查询生产的消息
+     * @param query
+     * @return
+     */
+    Result<MqFrom> getMqFrom(MqFromQuery query);
+
+    /**
+     * 插入消费的消息
+     * @param from
+     * @return
+     */
+    Result<MqTo> insertMqTo(MqTo from);
+
+    /**
+     * 更新的消息
+     * @param from
+     * @return
+     */
+    Result<Void> updateMqTo(MqTo from);
+
+    /**
+     * 查询消费的消息列表
+     * @param query
+     * @return
+     */
+    Result<ListWrapper<MqTo>> findMqTo(MqToQuery query);
+
+    /**
+     * 查询消费的消息
+     * @param query
+     * @return
+     */
+    Result<MqTo> getMqFrom(MqToQuery query);
+
 
 }
