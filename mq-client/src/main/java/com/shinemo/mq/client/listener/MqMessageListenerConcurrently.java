a9f9dff28aaeda269cd2cdd3503ac9a9e863efb9
zhangyan <578033593@qq.com>
2018-12-15 17:30:48 +0800

    fix

--- mq-client/src/main/java/com/shinemo/mq/client/listener/MqMessageListenerConcurrently.java
+++ mq-client/src/main/java/com/shinemo/mq/client/listener/MqMessageListenerConcurrently.java
@@ -46,22 +46,30 @@ public class MqMessageListenerConcurrently implements MessageListenerConcurrentl
 	 */
 	private boolean checkExpire = false;
 	
+	/**
+	 * 一天的毫秒数
+	 */
+	private final static long DAYMILILLS = 24*60*60*1000;
+	
 	//数据库操作 rpc类
 
 	@Override
 	public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs,ConsumeConcurrentlyContext context) {
 		log.info(Thread.currentThread().getName() + " Receive New Messages: " + msgs.size());
 		for(MessageExt msg:msgs){
-			
 			if(checkExpire) {
 				//判断过期 加log
-				continue;
+				long exprieMills = DAYMILILLS*expireTimesDays;
+				long now = System.currentTimeMillis();
+				if(now - msg.getBornTimestamp() > exprieMills) {
+					log.error("[mqExpire] message:{}",msg);
+					continue;
+				}
 			}
 			
 			if(checkRepeatMessage){
-				
+				//new 数据库操作的类 根据是否主服务 否则加入http
 				//先查询是否被消费 无则插入 有则直接返回
-				
 			}else{
 				log.info("message receive:"+msg.getMsgId());
 				mqMessageConsumerService.handleMessage(msg);
