82b0b1ee87f92b13b10b0550e7b01ddab1c4d7ff
zhangyan <578033593@qq.com>
2018-12-23 17:49:19 +0800

    fix

--- mq-server-core/src/main/java/com/shinemo/mq/core/configuration/CoreConfiguration.java
+++ mq-server-core/src/main/java/com/shinemo/mq/core/configuration/CoreConfiguration.java
@@ -1,17 +1,24 @@
 package com.shinemo.mq.core.configuration;
 
 
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 import com.shinemo.jce.common.config.CenterConfig;
 import com.shinemo.jce.common.config.ConsumerConfig;
 import com.shinemo.jce.common.config.ProviderConfig;
 import com.shinemo.mq.server.client.common.entity.InternalEventBus;
+import com.shinemo.mq.server.client.message.facade.MqServerFacadeService;
 
 import java.util.Map;
+import java.util.Set;
 
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 /**
  * core 配置类
  *
@@ -47,6 +54,26 @@ public class CoreConfiguration {
     public InternalEventBus internalEventBus() {
         return new InternalEventBus();
     }
+    
+    @Bean
+	@DependsOn("shineMoConfig")
+	public Map<String, String> appTypeAceProxyMap(@Qualifier("shineMoConfig") ShineMoConfig shineMoConfig) {
+		Map<String, Integer> siteId = shineMoConfig.getApplication().getSiteId();
+		Map<String, String> siteDomain = shineMoConfig.getJce().getSiteDomain();
+		Set<String> keys = Sets.intersection(siteId.keySet(), siteDomain.keySet());
+		Map<String, String> appTypeAceProxyMap = Maps.newHashMap();
+		Integer site;
+		String url;
+		for (String key : keys) {
+			site = siteId.get(key);
+			url = siteDomain.get(key);
+			if (site == null || StringUtils.isBlank(url)) {
+				continue;
+			}
+			appTypeAceProxyMap.put(String.valueOf(site), url + "/aceproxy/dispatch");
+		}
+		return appTypeAceProxyMap;
+	}
     /**
      * 实例化 consumer 配置
      *
@@ -56,9 +83,11 @@ public class CoreConfiguration {
      * @date 2018-08-15
      **/
     @Bean(initMethod = "init")
-    public ConsumerConfig consumerConfig(@Value("${shinemo.jce.consumer.url}") String url) {
+    @DependsOn("appTypeAceProxyMap")
+    public ConsumerConfig consumerConfig(@Value("${shinemo.jce.consumer.url}") String url,
+    					@Qualifier("appTypeAceProxyMap") Map<String,String> appTypeAceProxyMap) {
         ConsumerConfig config = new ConsumerConfig();
-        Map<String, String> urlMap;
+        config.setUrlMap(appTypeAceProxyMap);
         return config;
     }
     /**
