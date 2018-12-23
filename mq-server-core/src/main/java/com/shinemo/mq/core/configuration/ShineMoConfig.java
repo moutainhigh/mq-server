bad0715be454abe921d7283e79c6924131861c56
zhangyan <578033593@qq.com>
2018-12-23 17:56:15 +0800

    fix

---
+++ mq-server-core/src/main/java/com/shinemo/mq/core/configuration/ShineMoConfig.java
@@ -0,0 +1,53 @@
+package com.shinemo.mq.core.configuration;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.Map;
+
+/**
+ * Created by harold on 11/21/17.
+ */
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "shinemo")
+public class ShineMoConfig {
+	
+	
+	@Valid
+	@NotNull
+	private Application application;
+
+	@Valid
+	@NotNull
+	private Jce jce;
+
+	@Valid
+	@NotNull
+	private Mq mq;
+
+	@Data
+	public static class Application {
+		@NotNull
+		private String domain;
+		@NotNull
+		private Map<String, Integer> siteId;
+	}
+
+	@Data
+	public static class Jce {
+		@NotNull
+		private Map<String, String> siteDomain;
+	}
+
+	@Data
+	public static class Mq {
+		@NotNull
+		private String address;
+		
+	}
+}
